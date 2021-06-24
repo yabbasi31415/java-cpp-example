a40721411b1642946f6790f32fb8d1333718a0d2
Yumna Abbasi <yumna.abbasi@emumba.com>
2021-06-25 02:14:45 +0500

    Reading text file in Java

--- java/src/app/Main.java
+++ java/src/app/Main.java
@@ -1,18 +1,30 @@
 package app;
-
 import cpplib.Calculator;
 
-public class Main {
+import java.io.*;
+public class Main
+{
+  public static void main(String[] args) throws Exception
+  {
+ 
+  File file = new File("/home/emumba/emumba/JavaBindings/JNI_forked/java-cpp-example/java/src/app/config.json");
+  
+  BufferedReader br = new BufferedReader(new FileReader(file));
+  
+  String st;
 
-	public static void main(String[] args) {
-		Calculator calc = new Calculator();
+  System.out.println("Reading config file");
+  while ((st = br.readLine()) != null)
+    System.out.println(st);
 
-		System.out.println("Starting Calculator");
+    Calculator calc = new Calculator();
+
+		System.out.println("\n\nStarting Calculator");
 		calc.add_to_memory(15);
 		System.out.println("32 + 18 = " + calc.add(32, 18));
 		System.out.println("27 - 10 = " + calc.subtract(27,10));
 		System.out.println("Square of 9 = " + calc.square(9));
 		System.out.println("12 * 5 = " + calc.multiply(12,5));
 		System.out.println("Recalled from memory = " + calc.recall_memory());
-	}
+  }
 }
