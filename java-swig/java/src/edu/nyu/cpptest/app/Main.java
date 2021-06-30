82261d5f0e6817d23a49b6e461e99f9f5b945b92
Yumna Abbasi <yumna.abbasi@emumba.com>
2021-07-01 00:20:18 +0500

    Removed jni directory, added logging in mainn

--- java-swig/java/src/edu/nyu/cpptest/app/Main.java
+++ java-swig/java/src/edu/nyu/cpptest/app/Main.java
@@ -1,18 +1,27 @@
 package edu.nyu.cpptest.app;
+import java.util.logging.*;
 
 import edu.nyu.cpptest.cpplib.Calculator;
 
 public class Main {
-
 	public static void main(String[] args) {
+
+		Logger logger = Logger.getLogger(Main.class.getName());
+		logger.setLevel(Level.INFO);
+
+		ConsoleHandler handler = new ConsoleHandler();
+        handler.setLevel(Level.INFO);
+        logger.addHandler(handler);
+
 		Calculator calc = new Calculator();
 
-		System.out.println("Starting Calculator");
+		logger.log(Level.INFO, "Starting Calculator");
 		calc.add_to_memory(15);
-		System.out.println("32 + 18 = " + calc.add(32, 18));
-		System.out.println("27 - 10 = " + calc.subtract(27,10));
-		System.out.println("Square of 9 = " + calc.square(9));
-		System.out.println("12 * 5 = " + calc.multiply(12,5));
-		System.out.println("Recalled from memory = " + calc.recall_memory());
+
+		logger.log(Level.FINE, "32 + 18 = " + calc.add(32, 18));
+		logger.log(Level.FINE, "27 - 10 = " + calc.subtract(27,10));
+		logger.log(Level.FINE, "Square of 9 = " + calc.square(9));
+		logger.log(Level.FINE, "12 * 5 = " + calc.multiply(12,5));
+		logger.log(Level.FINE, "Recalled from memory = " + calc.recall_memory());
 	}
 }
